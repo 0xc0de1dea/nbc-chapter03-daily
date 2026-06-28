d603f43f2d39d0a71c549ce043415cf6a2c5e4fa
0xc0de1dea <0xc0de1dea@gmail.com>
2026-06-28 21:30:15 +0900

    first commit

---
+++ src/main/java/com/example/chapter03daily/Chapter03DailyApplication.java
@@ -0,0 +1,13 @@
+package com.example.chapter03daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter03DailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter03DailyApplication.class, args);
+    }
+
+}
